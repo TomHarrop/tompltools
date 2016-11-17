01524d2c9bc279c22fbe3ab6e93c0989db7cc586
Tom Harrop <twharrop@gmail.com>
2016-11-17 14:50:26 +1300

    can't specify my own package as a dependency :(

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 
 setup(
     name='tompltools',
-    version='0.0.1',
+    version='0.0.2',
     description='Tom\'s python3 functions for ruffus pipelines',
     url='https://github.com/TomHarrop/tompltools',
     author='Tom Harrop',
@@ -14,7 +14,7 @@ setup(
     license='GPL-3',
     packages=find_packages(),
     install_requires=[
-        'tompytools>=0.03',
+        #'tompytools>=0.03',
         'datetime>=4.1.1'
         'os', 'subprocess', 're', 'tempfile'
     ],
